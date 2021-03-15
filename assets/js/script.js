743d861afb1dbfeb12bdeb98f68ed30dd6dbb2be
Jon <artolajon2@gmail.com>
2021-03-15 19:03:25 +0100

    Level progression

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,7 +63,7 @@ const wait = async (ms) => {
 }
 
 const setSmallerSpawnTime = () =>{
-  spawnTime -= spawnTime * 0.05;
+  spawnTime -= spawnTime * 0.1;
   if (spawnTime < SPAWN_TIME_MIN){
     spawnTime = SPAWN_TIME_MIN;
   }
@@ -83,6 +83,16 @@ const hideElements = (className, deleteNode = false) => {
   });
 }
 
+const hideElement = (id, deleteNode = false) => {
+  let element = document.getElementById(id);
+  if(deleteNode){
+    document.body.removeChild(element);
+  }else{
+    if (!element.classList.contains('hidden'))
+      element.classList.add('hidden')
+  }
+}
+
 
 const setScore = (score) => {
   let element = document.getElementById('score');
@@ -90,21 +100,47 @@ const setScore = (score) => {
 }
 
 
-const showEnd = () => {
+const showElement = (name) => {
+  let div = document.getElementById(name);
+  div.classList.remove('hidden');
+}
+
+const diableReplay=()=>{
   let endMenu = document.getElementById("end");
   let replayButton = endMenu.getElementsByTagName("button");
   replayButton[0].disabled = true;
 
-  endMenu.classList.remove('hidden');
   setTimeout(() => replayButton[0].disabled = false, 1000);
+}
+
+const updateSpawnTimeText = () =>{
+  let perSecond=1000/spawnTime;
+  document.getElementById("spawn-time").textContent = `${perSecond.toFixed(1)}/1s`;
+}
+
+
+const nextLevel = () => {
+  updateSpawnTimeText();
+  setSmallerSpawnTime();
+
+}
+
+const levelController = () => {
+  updateSpawnTimeText();
+  setInterval(()=> {
+    nextLevel();
+  }, 5000);
 }
 
 const startGame = async () =>{
   hideElements("menu");
+  
 
   spawnTime = SPAWN_TIME_STARTING;
   let startDatetime = new Date();
   enemyNumber = 0;
+  levelController();
+  showElement("game-status");
 
   while(true){
     addADog();
@@ -112,11 +148,12 @@ const startGame = async () =>{
       break;
     }
     await wait(spawnTime);
-    setSmallerSpawnTime();    
   }
   hideElements("dog", true);
   let endDatetime = new Date();
   var playedTime = (endDatetime - startDatetime) / 1000;
+  hideElement("game-status");
   setScore(playedTime);
-  showEnd();
+  diableReplay()
+  showElement("end");
 }
